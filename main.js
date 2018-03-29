0ebbfaef25caa6c6133bc8b308167d33262a4b48
Annie Zhang <zz229@cornell.edu>
2018-03-29 09:06:48 -0400

    satellite parsing complete

--- main.js
+++ main.js
@@ -1,18 +1,56 @@
-function parseLine(line) {
+function parseGdpRow(row) {
     var gdpDict = {};
     for(var i = 1960; i<2017; i++){
-        gdpDict[i] = Number(line[i]);
+        gdpDict[i] = Number(row[i]);
     }
 
     return {
-        countryName: line["Country Name"],
-        countryCode: line["Country Code"],
+        countryName: row["Country Name"],
+        countryCode: row["Country Code"],
         GDP: gdpDict
     };
 }
 
+function gdpCallback(err, data) {
+    console.log(data);
+}
+
+function parseSatelliteRow(row) {
+    var launchMass;
+    var orbitsPerDay;
+    var perigee, apogee, altitude;
+    if (row["Launch Mass (kg.)"] > 0) {
+        launchMass = row["Launch Mass (kg.)"];
+    } else {
+        launchMass = "unknown";
+    }
+    if (row["Period (minutes)"].includes("days")) {
+        orbitsPerDay = 1/(row["Period (minutes)"].substring(0,row["Period (minutes)"].length - 5));
+    } else {
+        orbitsPerDay = row["Period (minutes)"]/1440;
+    }
+    perigee = Number(row["Perigee (km)"].replace(",",""));
+    apogee = Number(row["Apogee (km)"].replace(",",""));
+    altitude = (perigee + apogee)/2;
+
+    return {
+        name: row["Current Official Name of Satellite"],
+        countryOperator: row["Country of Operator/Owner"],
+        countryContractor: row["Country of Contractor"],
+        user: row["Users"],
+        purpose: row["Purpose"],
+        altitude: altitude,
+        launchMass: launchMass,
+        launchDate: row["Date of Launch"],
+        orbitsPerDay: orbitsPerDay
+    }
+}
+
+function satelliteCallback(err, data) {
+    console.log(data);
+}
+
 // Load data from csv
 // Call callbacks to generate images
-d3.csv("gdpGood.csv", parseLine, function(err, data) {
-    console.log(data);
-});
+d3.csv("gdpGood.csv", parseGdpRow, gdpCallback);
+d3.csv("satellites.csv", parseSatelliteRow, satelliteCallback);
