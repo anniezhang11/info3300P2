b4179a3f260b9e47d62e348eb534f8584a1b997e
Annie Zhang <zz229@cornell.edu>
2018-04-09 16:58:25 -0400

    basic upside down bar chart completed

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var gdpData;
+var satelliteData;
+var topTenData = [];
+
 function parseGdpRow(row) {
     var gdpDict = {};
     for(var i = 1960; i<2017; i++){
@@ -12,13 +16,21 @@ function parseGdpRow(row) {
 }
 
 function gdpCallback(err, data) {
-    console.log(data);
+    gdpData = data;
 }
 
 function parseSatelliteRow(row) {
     var launchMass;
     var orbitsPerDay;
     var perigee, apogee, altitude;
+    var country;
+    if (row["Country of Operator/Owner"] == "USA") {
+        country = "United States";
+    } else if (row["Country of Operator/Owner"] == "Russia") {
+        country = "Russian Federation";
+    } else {
+        country = row["Country of Operator/Owner"];
+    }
     if (row["Launch Mass (kg.)"] > 0) {
         launchMass = row["Launch Mass (kg.)"];
     } else {
@@ -35,7 +47,7 @@ function parseSatelliteRow(row) {
 
     return {
         name: row["Current Official Name of Satellite"],
-        countryOperator: row["Country of Operator/Owner"], //gdp
+        countryOperator: country, //gdp
         countryContractor: row["Country of Contractor"],
         user: row["Users"],
         purpose: row["Purpose"],
@@ -46,12 +58,77 @@ function parseSatelliteRow(row) {
     }
 }
 
+// sorts byCountry by number of satellites, most to least
+function customCompare(a,b) {
+    if (a.values.length < b.values.length) {
+        return 1;
+    }
+    if (a.values.length > b.values.length) {
+        return -1;
+    }
+    return 0;
+}
+
 function satelliteCallback(err, data) {
-    console.log(data);
+    satelliteData = data;
+    // console.log(gdpData);
+    // console.log(satelliteData);
     var byCountry = d3.nest()
         .key(function(d){return d.countryOperator;})
         .entries(data);
-    console.log(byCountry);
+    byCountry = byCountry.sort(customCompare);
+
+    // extracting the ten countries with the most satellites
+    var idx = 0;
+    var count = 0;
+    var topTen = [];
+    var totalSatellites = 0;
+    while (count < 10) {
+        if (byCountry[idx].key !== "Multinational" && byCountry[idx].key !== "ESA") {
+            topTen.push(byCountry[idx]);
+            count++;
+            totalSatellites += byCountry[idx].values.length;
+        }
+        idx++;
+    }
+    console.log(topTen);
+
+    // organizing top ten country data for the gdp bar chart
+    var thisCountryData;
+    for (var i=0; i < 10; i++) {
+        thisCountryData = {
+            name: topTen[i].key,
+            numberSatellites: topTen[i].values.length,
+            proportionSatellites: (topTen[i].values.length/totalSatellites),
+            gdp: gdpData.find(x => x.countryName == topTen[i].key).GDP[2016]
+        };
+        topTenData.push(thisCountryData);
+    }
+    console.log(topTenData);
+
+    var svg = d3.select("#gdpBars");
+    var padding = 10;
+        margin = {top: 20, right: 20, bottom: 20, left: 20},
+        width = document.getElementById("left").offsetWidth - margin.left - margin.right,
+        height = document.getElementById("left").offsetHeight - margin.top - margin.bottom;
+    var x = d3.scaleBand().rangeRound([0, width]);
+        y = d3.scaleLinear().rangeRound([height, 0]);
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    x.domain(topTenData.map(function(d) { return d.name; }));
+    y.domain([0, d3.max(topTenData, function(d) { return d.gdp; })]);
+
+    g.selectAll(".bar")
+        .data(topTenData)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.name); })
+        .attr("y", function(d) { return 0; })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(d.gdp); });
+
+
 }
 
 // Load data from csv
