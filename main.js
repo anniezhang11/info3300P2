8b173f5c34604600e26d5ddd3963411680993160
Oren Michaely <orenmichaely@gmail.com>
2018-04-10 01:27:11 -0400

    initial circles and triangles

--- main.js
+++ main.js
@@ -47,19 +47,19 @@ function parseSatelliteRow(row) {
     altitude = (Number(perigee) + Number(apogee))/2;
 
     if (launchMass == "unknown") {
-        massDiam = 7;
+        massDiam = 15;
     } else if (Number(launchMass)>10000) {
-        massDiam = 11;
+        massDiam = 21;
     } else if (Number(launchMass) > 5000) {
-        massDiam = 9;
+        massDiam = 18;
     } else if (Number(launchMass) > 1000) {
-        massDiam = 7.5;
+        massDiam = 15;
     } else if (Number(launchMass) > 500) {
-        massDiam = 6;
+        massDiam = 12;
     } else if (Number(launchMass) > 100) {
-        massDiam = 4.5;
+        massDiam = 9;
     } else {
-        massDiam = 3;
+        massDiam = 6;
     }
 
     return {
@@ -160,27 +160,47 @@ function drawSatellites(data, x_scale) {
     var x_start, x_end, x_coord, y_coord;
     var y_scale = d3.scaleLinear()
             .rangeRound([1800,0]);
-    y_scale.domain([0, d3.max(data, function(d) { 
+    y_scale.domain([d3.max(data, function(d) { 
         return d3.max(d.satellites, function(s) {
             return s.altitude;
         });
-     })]);
+     }),0]);
     data.forEach(element => {
         x_start = x_scale(element.accumulateSatellites);
         x_end = x_scale(element.accumulateSatellites + element.proportionSatellites);
         element.satellites.forEach(satellite => {
             x_coord = Math.random() * (x_end-x_start);
             y_coord = y_scale(satellite.altitude);
-            console.log("altitude="+satellite.altitude);
-            console.log("y_coord="+ y_coord);
-            if(satellite.purpose == "Commercial"){
+            // console.log("altitude="+satellite.altitude);
+            // console.log("y_coord="+ y_coord);
+            // console.log(satellite.massDiam/2);
+            if(satellite.user == "Commercial"){
                 svgSat.append("circle")
                     .attr("cx", x_coord)
                     .attr("cy", y_coord)
                     .attr("r", (satellite.massDiam/2))
-                    .attr("fill", red)
-                    .attr("stroke", red);
+                    .attr("fill", "red")
+                    .attr("stroke", "red");
                     //we can add attributes and styles here
+            }else if(satellite.user == "Civil"){
+//                 <polygon fill="yellow" stroke="blue" stroke-width="2"
+// 3    points="05,30
+// 4            15,10
+// 5            25,30" />
+                var x1 = x_coord-(satellite.massDiam/2);
+                var h = Math.sqrt((satellite.massDiam*satellite.massDiam)-((satellite.massDiam/2)*(satellite.massDiam/2)));
+                var y1 = y_coord+(h/2);
+                var x2 = x_coord;
+                var y2 = y_coord-(h/2);
+                var x3 = x_coord+(satellite.massDiam/2);
+                var y3 = y_coord+(h/2);
+                // console.log("x1="+ x1);
+                var lineData = [{"x":x1, "y":y1}, {"x":x2, "y":y2}, {"x":x3, "y":y3}];
+                var lineFunction = d3.line()
+                    .x(function(d) { return d.x; })
+                    .y(function(s) { return s.y; });
+                svgSat.append("path")
+                    .attr("d", lineFunction(lineData));
             }
         });
     });
